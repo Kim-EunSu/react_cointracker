8211b4fef5fa1346841bb983effcfd9a72cc13af
Kim-EunSu <kes73640@gmail.com>
2023-04-19 15:29:55 +0900

    DefaultTheme 적용

--- src/pages/Coin.tsx
+++ src/pages/Coin.tsx
@@ -1,9 +1,14 @@
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function Coin() {
   const { coinId } = useParams();
   console.log(coinId);
-  return <h1>Coin :{coinId}</h1>;
+  return <Title>Coin :{coinId}</Title>;
 }
 
 export default Coin;
---
+++ src/theme.ts
@@ -0,0 +1,7 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  bgColor: "#d8e2dc",
+  textColor: "#6a6a6a",
+  accentColor: "#fcd5ce",
+};
